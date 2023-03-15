cab0e42fafeea380e2275805225b5972f42f9024
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-03-15 18:43:49 +0530

    Flutter 3D Model Viewer

---
+++ android/app/src/main/kotlin/com/example/flutter_3d_model_viewer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_3d_model_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Flutter 3D Model Viewer")),
+        body: ModelViewer(
+          backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
+          src: 'assets/Astronaut.glb', // a bundled asset file
+          alt: "A 3D model of an astronaut",
+          ar: true,
+          arModes: ['scene-viewer', 'webxr', 'quick-look'],
+          autoRotate: true,
+          cameraControls: true,
+          iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
+          disableZoom: true,
+        ),
+      ),
+    );
+  }
+}
